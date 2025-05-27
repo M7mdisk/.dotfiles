f3c1b0644f9137220f98cbf00aa995795bfd5422
Mohammad Iskandarany <M7md.k.isk@gmail.com>
2025-05-27 13:09:54 +0300

    more customized nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -653,7 +653,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'isort', { 'black', args = { '--line-length', '79' } } },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
@@ -784,10 +784,17 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
 
@@ -939,6 +946,9 @@ require('lazy').setup({
         desc = 'harpoon to file 5',
       },
     },
+  },
+  {
+    'github/copilot.vim',
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
